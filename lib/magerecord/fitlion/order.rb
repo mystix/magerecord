d9232ba05c47155c0012b03cb68c3c0d1b778ed4
Marc <xerokill@gmail.com>
2015-03-23 11:58:57 +0800

    added company-specific order default scope

---
+++ lib/magerecord/fitlion/order.rb
@@ -0,0 +1,17 @@
+module MageRecord
+  # add custom FitLion-specific order methods
+  class Order < ActiveRecord::Base
+    # ignore canceled orders
+    default_scope { where state: [:processing, :complete] }
+    scope :delivery, -> { where shipping_method: [:flatrate_flatrate :addon_addon] }
+    scope :collection, -> { where "#{self.class.table_name}.shipping_method LIKE 'selfcollect%'" }
+
+    def for_delivery?
+      %w{flatrate addon}.include? shipping_method.split('_').first
+    end
+
+    def for_collection?
+      shipping_method.include? 'selfcollect'
+    end
+  end
+end
