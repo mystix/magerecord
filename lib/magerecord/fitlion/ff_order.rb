680c8d52eebcbcc7ae40e3a18a27ff434fb54f7f
Marc <xerokill@gmail.com>
2015-10-08 15:57:00 +0800

    fixed typo: website_id -> store_id

--- lib/magerecord/fitlion/ff_order.rb
+++ lib/magerecord/fitlion/ff_order.rb
@@ -1,7 +1,7 @@
 module MageRecord
   # add custom fitnessfirst-specific order methods
   class FfOrder < Order
-    default_scope { where website_id: 2 }
+    default_scope { where store_id: 2 }
 
     belongs_to :club
     belongs_to :trainer
