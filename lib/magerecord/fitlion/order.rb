0aad53ff5e2e3fddedeb2362bbc70d913e2b2792
Marc <xerokill@gmail.com>
2016-12-07 12:56:37 +0800

    added activerecord scope "prepaid"

--- lib/magerecord/fitlion/order.rb
+++ lib/magerecord/fitlion/order.rb
@@ -5,6 +5,7 @@ module MageRecord
     default_scope { where state: [:processing, :complete] }
     scope :delivery, -> { where shipping_method: [:flatrate_flatrate, :addon_addon] }
     scope :collection, -> { where "#{Order.table_name}.shipping_method LIKE 'selfcollect%'" }
+    scope :prepaid, -> { joins(:payment).where("#{OrderPayment.table_name}.method NOT IN (?, ?)", 'cashondelivery', 'checkmo') }
 
     def for_delivery?
       %w{flatrate addon}.include? shipping_method.split('_').first
