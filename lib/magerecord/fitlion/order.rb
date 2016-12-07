d3d2c56e4fcf2ebf502f8812022f9232ac98ee7e
Marc <xerokill@gmail.com>
2016-12-07 14:14:25 +0800

    added scopes "town" and "residential"

--- lib/magerecord/fitlion/order.rb
+++ lib/magerecord/fitlion/order.rb
@@ -1,11 +1,20 @@
 module MageRecord
   # add custom FitLion-specific order methods
   class Order < ActiveRecord::Base
+    # mysql regex for postal codes that are in town
+    TOWN_REGEX = '^(0[1-9]|10|1[78]|2[2-4])'
+
     # ignore canceled orders
     default_scope { where state: [:processing, :complete] }
+
+    scope :prepaid, -> { joins(:payment).where("#{OrderPayment.table_name}.method NOT IN (?, ?)", 'cashondelivery', 'checkmo') }
+
     scope :delivery, -> { where shipping_method: [:flatrate_flatrate, :addon_addon] }
     scope :collection, -> { where "#{Order.table_name}.shipping_method LIKE 'selfcollect%'" }
-    scope :prepaid, -> { joins(:payment).where("#{OrderPayment.table_name}.method NOT IN (?, ?)", 'cashondelivery', 'checkmo') }
+
+    scope :town, -> { joins(:shipping_address).where("sales_flat_order_address.postcode RLIKE ?", TOWN_REGEX) }
+    scope :residential, -> { joins(:shipping_address).where("sales_flat_order_address.postcode NOT RLIKE ?", TOWN_REGEX) }
+
 
     def for_delivery?
       %w{flatrate addon}.include? shipping_method.split('_').first
